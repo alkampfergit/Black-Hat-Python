337cae195d77b556efb8d255759388b43083a66f
Gian Maria Ricci <alkampfer@nablasoft.com>
2019-08-19 19:21:56 +0200

    started porting to python 3

--- BHP-Code/Chapter2/udp-client.py
+++ BHP-Code/Chapter2/udp-client.py
@@ -6,10 +6,13 @@ target_port = 80
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+#Changed in version 3 of python, need to bind to port.
+client.bind((target_host, target_port))
+
 # send some data
-client.sendto("AAABBBCCC",(target_host, target_port))
+client.sendto(b"AAABBBCCC",(target_host, target_port))
 
 # recieve some data
 data, addr = client.recvfrom(4096)
 
-print data
+print(data)
