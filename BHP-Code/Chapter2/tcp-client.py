19c84c5f1915024792bd559fb673869cc51235ff
Gian Maria Ricci <alkampfer@nablasoft.com>
2019-08-19 19:58:24 +0200

    more modification

--- BHP-Code/Chapter2/tcp-client.py
+++ BHP-Code/Chapter2/tcp-client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "localhost"
+target_port = 5123
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- BHP-Code/Chapter2/tcp-server.py
+++ BHP-Code/Chapter2/tcp-server.py
@@ -1,8 +1,25 @@
 import socket
 import threading
 
+
+def handle_client(client_socket):
+    
+    # send something
+    client_socket.send(b"Connected\r\n")
+    
+    # print out what the client sends
+    request = client_socket.recv(1024)
+    
+    print ("[*] Reveived: %s" % request)
+    
+    # send back a packet
+    client_socket.send(b"ACK!\r\n")
+    
+    client_socket.close()
+
+#This is the main function
 bind_ip = "0.0.0.0"
-bind_port = 9999
+bind_port = 5123
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -10,27 +27,12 @@ server.bind((bind_ip, bind_port))
 
 server.listen(5)
 
-print "[*] Listening on %s:%d" % (bind_ip, bind_port)
-
-def handle_client(client_socket):
-    
-    # send something
-    client_socket.send("Connected\r\n")
-    
-    # print out what the client sends
-    request = client_socket.recv(1024)
-    
-    print "[*] Reveived: %s" % request
-    
-    # send back a packet
-    client_socket.send("ACK!\r\n")
-    
-    client_socket.close()
+print ("[*] Listening on %s:%d" % (bind_ip, bind_port))
     
 while True:
     client, addr = server.accept()
     
-    print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+    print ("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
     
     # spin up our client thread to handle incoming data
     client_handler = threading.Thread(target=handle_client,args=(client,))
